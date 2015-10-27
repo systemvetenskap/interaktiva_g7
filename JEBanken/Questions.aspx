7e0e7a308c401f3955b2d772a0069f21dcf287a2
Michael Byrskog <micke884@hotmail.com>
2015-10-27 14:13:18 +0100

    commit, nytt

---
+++ JEBanken/Questions.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Questions.aspx.cs" Inherits="JEBanken.Questions" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblQuestion" runat="server" Text="Fråga 1/25"></asp:Label><br />
+
+        <asp:RadioButton ID="RadioButton1" runat="server" GroupName="Svar" /><label for="radio1"></label><br />
+        <asp:RadioButton ID="RadioButton2" runat="server" GroupName="Svar" /><label for="radio1"></label><br />
+        <asp:RadioButton ID="RadioButton3" runat="server" GroupName="Svar" /><label for="radio1"></label><br />
+        <asp:RadioButton ID="RadioButton4" runat="server" GroupName="Svar" /><label for="radio1"></label><br />
+        <asp:RadioButton ID="RadioButton5" runat="server" GroupName="Svar" /><label for="radio1"></label><br />
+
+    </div>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Nästa" />
+    </form>
+</body>
+</html>
---
+++ JEBanken/Questions.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace JEBanken
+{
+    public partial class Questions : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+
+            xmldoc.Load(Server.MapPath("XmlQuestions.xml"));
+
+
+
+            XmlNodeList nodeListAnswer = xmldoc.SelectNodes("/categories/ethicandrules/question[@id='2']/answer/answer");
+            XmlNodeList nodeListCorrectAnswer = xmldoc.SelectNodes("/categories/ethicandrules/question[@id='1']/correctanswer");
+
+            lblQuestion.Text = xmldoc.SelectSingleNode("/categories/ethicandrules/question[@id='1']").FirstChild.InnerText;
+
+            RadioButton1.Text = nodeListAnswer[0].FirstChild.InnerText;
+            //RadioButton1.Text = xmldoc.SelectSingleNode("/categories/ethicandrules/question/answer/answer[@id='1']").FirstChild.InnerText;
+            RadioButton2.Text = nodeListAnswer[1].FirstChild.InnerText;
+            RadioButton3.Text = nodeListAnswer[2].InnerText;
+            RadioButton4.Text = nodeListAnswer[3].FirstChild.InnerText;
+            RadioButton5.Text = nodeListCorrectAnswer[0].FirstChild.InnerText;
+        }
+    }
+}
