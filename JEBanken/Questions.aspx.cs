4b2659b219d92d665f3f40cf13c27d0592f3b8b8
stefan85nikolic <stefangolman85@gmail.com>
2015-10-28 11:04:51 +0100

    radio numbers with no repetition

--- JEBanken/Questions.aspx.cs
+++ JEBanken/Questions.aspx.cs
@@ -36,5 +36,54 @@ namespace JEBanken
             
             Response.Write("Test");
         }
+
+        #region radionumbers 
+        public void getNumbers()
+        {
+            int[] m = RandomNumbers(1, 25, 5);
+            string[] questions = new string[25]; // Initialize.
+
+            for (int i = 0; i < m.Length; i++)
+            {
+                questions[i] = m[i].ToString();
+            }
+        }
+
+        public static int[] RandomNumbers(int min, int max)
+        {
+            return RandomNumbers(min, max, 2);
+        }
+        public static int[] RandomNumbers(int min, int max, int derangement)
+        {
+            if (min > max)
+            {
+                throw new Exception("The first parameter must be less (or equal) than the second.");
+            }
+            Random random = new Random();
+            int count = max - min; ;
+            int[] tempList = new int[count + 1];
+            int counter = 0;
+            for (int i = min; i <= max; i++)
+            {
+                tempList[counter] = i;
+                counter++;
+            }
+
+            for (int i = 0; i < derangement; i++)
+                for (int j = 0; j < count; j++)
+                {
+                    int k = random.Next(0, count + 1);
+                    int l = random.Next(0, count + 1);
+                    if (k != l)
+                    {
+                        //Swap TempList[k] with TempList[l]
+                        tempList[k] += tempList[l];
+                        tempList[l] = tempList[k] - tempList[l];
+                        tempList[k] = tempList[k] - tempList[l];
+                    }
+                }
+            return tempList;
+        }
+        #endregion
     }
 }
